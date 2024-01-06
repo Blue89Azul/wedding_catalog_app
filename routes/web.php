cb9613861d992cb1388adadf8e877b9752df2bb1
Blue89Azul <life.89.blue@gmail.com>
2024-01-06 11:36:23 +0900

    rename login blade file

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/top', function () {
-    return view('top');
+    return view('login');
 });
