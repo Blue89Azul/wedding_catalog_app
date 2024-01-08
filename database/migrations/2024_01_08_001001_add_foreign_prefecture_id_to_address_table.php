663e6b137b4aefbb90b4ee4c58516d241f453b05
Blue89Azul <life.89.blue@gmail.com>
2024-01-08 09:15:24 +0900

    add foreign_key_constrains

---
+++ database/migrations/2024_01_08_001001_add_foreign_prefecture_id_to_address_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('addresses', function (Blueprint $table) {
+            $table->unsignedBigInteger('prefecture_id')->after('post_code');
+            $table->foreign('prefecture_id')->references('id')->on('prefectures');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('addresses', function (Blueprint $table) {
+            $table->dropForeign('addresses_prefecture_id_foreign');
+            $table->dropColumn('prefecture_id');
+        });
+    }
+};
