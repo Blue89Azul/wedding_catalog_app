b0dac762230ee5909191911ba2dd4b779f5de1ed
Blue89Azul <life.89.blue@gmail.com>
2024-02-06 15:11:27 +0900

    modifed for loading css and js files

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,17 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
+                // CSS
+                'resources/css/main.css',
+                'resources/css/chat.css',
+                'resources/css/lp.css',
                 'resources/sass/app.scss',
+
+                // JS
                 'resources/js/app.js',
+                'resources/js/card_button.js',
+                'resources/js/wedding_photos_slider.js',
+                'resources/js/image_button.js'
             ],
             refresh: true,
         }),
