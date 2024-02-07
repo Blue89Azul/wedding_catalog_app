9121335d45e279940fb2660d843183bd53f3ca4c
Blue89Azul <life.89.blue@gmail.com>
2024-02-07 11:19:49 +0900

    add insert method

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Address extends Model
 {
     use HasFactory;
+
+    public function insert(array $insert_data)
+    {
+        $this->post_code     = $insert_data['post_code'];
+        $this->prefecture_id = $insert_data['prefecture_id'];
+        $this->city          = $insert_data['city'];
+        $this->street        = $insert_data['street'];
+        $this->building      = $insert_data['building'];
+
+        $this->save();
+    }
 }
