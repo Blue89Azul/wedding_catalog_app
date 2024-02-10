ff0fb7c4c290a164c15eb008b5f39c5244934a00
Blue89Azul <life.89.blue@gmail.com>
2024-02-10 20:20:44 +0900

    add UserRepository

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -6,21 +6,24 @@ use App\Models\Address;
 use App\Models\User;
 
 class UserRepository {
-    private $prefectureModel;
-    private $addressModel;
-    private $userModel;
+    private Prefecture $prefecture;
+    private Address $address;
+    private User $user;
 
-    public function __construct(
-        PrefectureModel $prefectureModel,
-        AddressModel $addressModel,
-        UserModel $userModel
-    ) {
-        $this->prefectureModel = $prefectureModel;
-        $this->addressModel    = $addressModel;
-        $this->userModel       = $addressModel;
+    public function __construct(Prefecture $prefecture, Address $address, User $user) 
+    {
+        $this->prefecture = $prefecture;
+        $this->address    = $address;
+        $this->user       = $user;
     }
 
-    public function bulkSave(array $userList) {
+    // Transaction
+    public function insert(array $userInfo): void
+    {
+        $prefectureId = $prefecture->getIdByName($userInfo['prefecture']);
+        $userInfo['prefecture_id'] = $prefectureId;
 
+        $address->insert($userInfo);
+        
     }
 }
