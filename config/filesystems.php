6175f01c059127abd143bd0955f7e382ea23cdae
Blue89Azul <life.89.blue@gmail.com>
2024-03-10 16:02:26 +0900

    add master disk

--- config/filesystems.php
+++ config/filesystems.php
@@ -32,8 +32,14 @@ return [
 
         'local' => [
             'driver' => 'local',
-            'root' => storage_path('app'),
+            'root'   => storage_path('app'),
             'throw' => false,
+        ],
+
+        'master' => [
+            'driver' => 'local',
+            'root'   => storage_path('master'),
+            'throw'  => false,
         ],
 
         'public' => [
